4d5ea57289f005b37a4b9728fa56c24263a6db46
BaeCheolHan <joker10421@gmail.com>
2023-06-12 14:26:39 +0900

    카카오 로그인 연동중

--- src/views/FrontMain.vue
+++ src/views/FrontMain.vue
@@ -1,11 +1,12 @@
 <template>
   <TheHeader/>
   <div>
-    this is main
+    <section class="test">
+      <div v-on:click="kakaoLoginBtn">카카오 연동</div>
+    </section>
   </div>
   <TheFooter></TheFooter>
 </template>
-
 <script>
 import TheHeader from "@/components/TheHeader";
 import TheFooter from "@/components/TheFooter";
@@ -17,9 +18,53 @@ export default {
 
     }
   },
+  beforeCreate() {
+  },
   methods: {
+    kakaoLoginBtn:function(){
 
+      window.Kakao.init('41ad0cc682fed5cec0c7799409ef78ca') // Kakao Developers에서 요약 정보 -> JavaScript 키
+
+      if (window.Kakao.Auth.getAccessToken()) {
+        window.Kakao.API.request({
+          url: '/v1/user/unlink',
+          success: function (response) {
+            console.log(response)
+          },
+          fail: function (error) {
+            console.log(error)
+          },
+        })
+        window.Kakao.Auth.setAccessToken(undefined)
+      }
+
+
+      window.Kakao.Auth.login({
+        success: function () {
+          window.Kakao.API.request({
+            url: '/v2/user/me',
+            data: {
+              property_keys: ["kakao_account.email"]
+            },
+            success: async function (response) {
+              console.log(response);
+            },
+            fail: function (error) {
+              console.log(error)
+            },
+          })
+        },
+        fail: function (error) {
+          console.log(error)
+        },
+      })
+    }
   }
 
 }
 </script>
+
+<style scoped>
+.test{ display:flex; justify-content: center; align-items: center; height:100vh; }
+div{ width: 200px; height:40px; background-color:#fdd101; color:white; display:flex; align-items: center; justify-content: center; cursor:pointer; }
+</style>
