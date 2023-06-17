4e8f810019caa3c89f03216659501eca35d70e62
BaeCheolHan <joker10421@gmail.com>
2023-06-17 10:02:53 +0900

    카카오 로그인 연동중

--- src/views/FrontMain.vue
+++ src/views/FrontMain.vue
@@ -28,16 +28,10 @@ export default {
   },
   methods: {
     kakaoLoginBtn: async function(){
-      this.isClickLoginBtn = true;
+
       let res = await this.axios.get('/login/kakao')
-      // let iframe = document.createElement('iframe');
-      // console.log(document.getElementsByClassName('iframe-wrap'))
       console.log(res.data)
-      this.htmlContents = res.data;
-      // document.getElementsByClassName('iframe-wrap')[0].innerHTML = res.data;
-      // iframe.contentWindow.document.open();
-      // iframe.contentWindow.document.write(res.data);
-      // iframe.contentWindow.document.close();
+      location.href = res.data.loginUri
     },
   }
 
