c92b8e01aa3fc5ac5f11284fa4dc8f537ea4cfa9
BaeCheolHan <joker10421@gmail.com>
2023-06-22 10:18:05 +0900

    증권사 검색 select box 추가

--- src/views/account/SaveBankAccount.vue
+++ src/views/account/SaveBankAccount.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="content">
     <h2>계좌 등록</h2>
-    <div v-for="bank in banks" v-bind:key="bank">
-      <img class="bank-icon" :src="'./bank-icons/'.concat(bank.bankCode).concat('.jpg')" @error="replaceBankDefaultImg">
-      {{bank.bankName}} - {{bank.bankCode}}
+    <div class="mg-t-10">
+      <div class="searchSelect">
+        <input class="form-control" placeholder="증권사를 검색해주세요"  @focus="channelSelectFocus" @blur="closeDropDown" v-model="bankName" @keyup="searchBank">
+        <i class="ti-angle-down"></i>
+      </div>
+      <ul class="searchSelectBox" @blur="closeDropDown" @focus="channelSelectFocus">
+        <li  v-for="bank in copiedBanks" v-bind:key="bank">
+          <img class="bank-icon" :src="'./bank-icons/'.concat(bank.bankCode).concat('.jpg')" @error="replaceBankDefaultImg">
+          <span>{{bank.bankName}}</span>
+        </li>
+      </ul>
     </div>
+
   </div>
 </template>
 
@@ -17,17 +26,31 @@ export default {
   data: function () {
     return {
       banks: null,
+      copiedBanks: null,
+      bankName: null,
     }
   },
   created: async function () {
     let res = await this.axios.get("/api/banks");
     this.banks = res.data;
+    this.copiedBanks = this.banks.slice();
+    this.closeDropDown();
   },
   methods: {
     replaceBankDefaultImg(e) {
-
       e.target.src = './bank-icons/default-bank.png';
-    }
+    },
+    closeDropDown: function () {
+      document.getElementsByClassName('searchSelectBox')[0].style.display = "none";
+    },
+    channelSelectFocus: function () {
+      document.getElementsByClassName('searchSelectBox')[0].style.display = "";
+    },
+    searchBank: function () {
+      this.copiedBanks = this.banks.filter(item => {
+        return item.bankName.includes(this.bankName)
+      });
+    },
   }
 };
 </script>
