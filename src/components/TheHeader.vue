fea6d82eaf2b99f0fdce9a97b365b66b626e0de5
BaeCheolHan <joker10421@gmail.com>
2023-07-05 22:20:46 +0900

    환율정보 추가

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,25 +1,33 @@
 <template>
-  <header>
+    <header>
 
-  </header>
-  <div v-if="userInfo" class="profile-wrap">
-    <img class="profile-thumbnail" :src="userInfo.profile.thumbnail_image_url">
-    <div class="profile-nickname">
-      <span>{{ userInfo.profile.nickname }} ( {{userInfo.email}} )</span>
+    </header>
+    <div v-if="userInfo" class="flex header">
+        <div class="profile-wrap">
+            <img class="profile-thumbnail" :src="userInfo.profile.thumbnail_image_url">
+            <div class="profile-nickname">
+                <span>{{ userInfo.profile.nickname }}</span>
+            </div>
+        </div>
+        <div class="exchange-rate-wrap">
+            <p class="red font-weight-bold"> {{ userInfo.exchangeRate.basePrice }}원  ({{ userInfo.exchangeRate.date }} :
+                {{ userInfo.exchangeRate.time }})</p>
+            <span class="mg-r-45">살때 : {{ userInfo.exchangeRate.cashBuyingPrice }}</span>
+            <span>팔때 : {{ userInfo.exchangeRate.cashSellingPrice }}</span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-      userInfo: null,
-    }
-  },
-  created() {
-    this.userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
-  },
+    data: function () {
+        return {
+            userInfo: null,
+        }
+    },
+    created() {
+        this.userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
+    },
 
 }
 </script>
