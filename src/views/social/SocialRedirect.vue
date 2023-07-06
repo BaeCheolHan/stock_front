c312cbc787fe6963eefc53942d98fa0f7631111c
BaeCheolHan <joker10421@gmail.com>
2023-07-06 09:58:29 +0900

    로그인 redirect 시 예외처리

--- src/views/social/SocialRedirect.vue
+++ src/views/social/SocialRedirect.vue
@@ -12,11 +12,16 @@ export default {
     }
   },
   created: async function () {
-    const code = this.$route.query.code;
-    let response = await this.axios.get('/sns/login/kakao?code='.concat(code));
-    if (response.data) {
-      this.$store.commit('setUserInfo', JSON.stringify(response.data))
-      sessionStorage.setItem('userInfo', JSON.stringify(response.data))
+    try {
+      const code = this.$route.query.code;
+
+      let response = await this.axios.get('/sns/login/kakao?code='.concat(code));
+      if (response.data) {
+        this.$store.commit('setUserInfo', JSON.stringify(response.data))
+        sessionStorage.setItem('userInfo', JSON.stringify(response.data))
+        location.href = '/'
+      }
+    } catch (e) {
       location.href = '/'
     }
 
