a51dd9de3ce414ac5e05cbf040dac4098a0455fb
BaeCheolHan <joker10421@gmail.com>
2023-07-18 19:09:12 +0900

    아이콘 추가

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,7 @@
 <template>
   <footer class="mg-t-20">
     <a target="_blank" href="https://icons8.com/icon/3c34EQ6PKOT1/%EB%B0%B0%EB%8B%B9%EA%B8%88">배당금</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+    <br/>
     <a target="_blank" href="https://icons8.com/icon/68906/collectibles">Collectibles</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
   </footer>
 </template>
