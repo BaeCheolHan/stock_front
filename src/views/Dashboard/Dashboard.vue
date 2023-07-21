d8ae204271635cdc11dcc4c0bbe785a27673364e
BaeCheolHan <joker10421@gmail.com>
2023-07-21 22:27:24 +0900

    dashboard 마크업 정리

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -40,24 +40,18 @@
       </div>
       <!-- tab 내용 영역 -->
       <div class="pd-5" v-show="!checkSpin">
-        <v-card>
           <v-window v-model="bankAccountTab">
             <!-- 계좌 전체 영역 -->
-            <v-window-item :value="'all'">
-              <v-container fluid>
+              <v-container fluid :value="'all'">
                 <!-- 주식 종목 item 영역 -->
                 <StockBox :stocks="stocks"/>
               </v-container>
-            </v-window-item>
             <!-- 개별 계좌 영역 -->
-            <v-window-item v-for="account in userInfo.bankAccounts" :key="account.id" :value="account.id">
-              <v-container fluid>
+              <v-container fluid v-for="account in userInfo.bankAccounts" :key="account.id" :value="account.id">
                 <!-- 주식 종목 item 영역 -->
                 <StockBox :stocks="stocks"/>
               </v-container>
-            </v-window-item>
           </v-window>
-        </v-card>
       </div>
       <div v-if="!userInfo.bankAccounts || userInfo.bankAccounts.length === 0" class="account-wrap">
         <div class="empty-account" @click="openRegAccountPop()">
