468f4b550f86d7f13d2b8902c6aac6d32227218b
BaeCheolHan <joker10421@gmail.com>
2023-11-03 22:43:45 +0900

    indexchart 기간별

--- src/components/dashboard/IndexChartArea.vue
+++ src/components/dashboard/IndexChartArea.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="flex mg-t-5" style="justify-content: right;" :style="isMobileFont()">
-  <span>* 각 지수는 3분 주기로 갱신합니다.</span>
+    <span>* 각 지수는 3분 주기로 갱신합니다.</span>
   </div>
-  <div class="mg-t-5">
+  <div class="flex mg-t-5" style="justify-content: right;">
+    <div class="flex" :style="isMobileFont()">
+      <button class="mg-r-10" :class="{'redBtn' : chartType === 'D', 'border-radius-8' : chartType !== 'D'}"
+              @click="changeChartType('D')">일별
+      </button>
+      <button class="mg-r-10" :class="{'redBtn' : chartType === 'W', 'border-radius-8' : chartType !== 'W'}"
+              @click="changeChartType('W')">주별
+      </button>
+      <button class="mg-r-10" :class="{'redBtn' : chartType === 'M', 'border-radius-8' : chartType !== 'M'}"
+              @click="changeChartType('M')">월별
+      </button>
+      <button class="mg-r-10" :class="{'redBtn' : chartType === 'Y', 'border-radius-8' : chartType !== 'Y'}"
+              @click="changeChartType('Y')">년별
+      </button>
+    </div>
+  </div>
+  <div class="mg-t-10">
     <div class="flex" style="flex-wrap: wrap; max-width: 90%; margin: 0 auto; justify-content: space-around">
       <div style="min-width: 165px; width: 40%" v-if="kospi">
         <div class="flex" style="justify-content: flex-start; align-items: center">
@@ -108,6 +124,7 @@ export default {
       kosdaq: null,
       snp: null,
       nasdaq: null,
+      chartType: 'D',
       kospiSeries: [{
         data: []
       }],
@@ -167,30 +184,46 @@ export default {
       },
     }
   },
+  watch: {
+    'chartType': async function () {
+      console.log("!")
+      await this.getIndexChartData()
+    }
+  },
   created: async function () {
-    let res = await this.axios.get('/api/dashboard/index-chart');
-    this.kospi = res.data.kospi;
-    this.kosdaq = res.data.kosdaq;
-    this.snp = res.data.snp;
-    this.nasdaq = res.data.nasdaq;
-    this.kospi.output2.forEach(item => this.kospiSeries[0].data.push({
-      x: item.stck_bsop_date,
-      y: [item.bstp_nmix_oprc, item.bstp_nmix_hgpr, item.bstp_nmix_lwpr, item.bstp_nmix_prpr]
-    }))
-    this.kosdaq.output2.forEach(item => this.kosdaqSeries[0].data.push({
-      x: item.stck_bsop_date,
-      y: [item.bstp_nmix_oprc, item.bstp_nmix_hgpr, item.bstp_nmix_lwpr, item.bstp_nmix_prpr]
-    }))
-    this.snp.output2.forEach(item => this.snpSeries[0].data.push({
-      x: item.stck_bsop_date,
-      y: [item.ovrs_nmix_oprc, item.ovrs_nmix_hgpr, item.ovrs_nmix_lwpr, item.ovrs_nmix_prpr]
-    }))
-    this.snp.output2.forEach(item => this.nasdaqSeries[0].data.push({
-      x: item.stck_bsop_date,
-      y: [item.ovrs_nmix_oprc, item.ovrs_nmix_hgpr, item.ovrs_nmix_lwpr, item.ovrs_nmix_prpr]
-    }))
+    await this.getIndexChartData();
   },
   methods: {
+    async getIndexChartData() {
+      let res = await this.axios.get('/api/dashboard/index-chart/'.concat(this.chartType));
+      this.kospi = res.data.kospi;
+      this.kosdaq = res.data.kosdaq;
+      this.snp = res.data.snp;
+      this.nasdaq = res.data.nasdaq;
+      this.kospiSeries[0].data = [];
+      this.kosdaqSeries[0].data = [];
+      this.snpSeries[0].data = [];
+      this.nasdaqSeries[0].data = [];
+      this.kospi.output2.forEach(item => this.kospiSeries[0].data.push({
+        x: item.stck_bsop_date,
+        y: [item.bstp_nmix_oprc, item.bstp_nmix_hgpr, item.bstp_nmix_lwpr, item.bstp_nmix_prpr]
+      }))
+      this.kosdaq.output2.forEach(item => this.kosdaqSeries[0].data.push({
+        x: item.stck_bsop_date,
+        y: [item.bstp_nmix_oprc, item.bstp_nmix_hgpr, item.bstp_nmix_lwpr, item.bstp_nmix_prpr]
+      }))
+      this.snp.output2.forEach(item => this.snpSeries[0].data.push({
+        x: item.stck_bsop_date,
+        y: [item.ovrs_nmix_oprc, item.ovrs_nmix_hgpr, item.ovrs_nmix_lwpr, item.ovrs_nmix_prpr]
+      }))
+      this.snp.output2.forEach(item => this.nasdaqSeries[0].data.push({
+        x: item.stck_bsop_date,
+        y: [item.ovrs_nmix_oprc, item.ovrs_nmix_hgpr, item.ovrs_nmix_lwpr, item.ovrs_nmix_prpr]
+      }))
+    },
+    changeChartType(val) {
+      this.chartType = val;
+    },
     isMobile() {
       const info = navigator.userAgent;
       let flag = false;
@@ -205,7 +238,7 @@ export default {
       return flag;
     },
     isMobileFont() {
-      if(this.isMobile()) {
+      if (this.isMobile()) {
         return 'font-size: 10px;'
       } else {
         return '';
